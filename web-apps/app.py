4404d84ecb2500ec51872587d1d8b0482f8bc41a
FuzailN <129302212+FuzailN@users.noreply.github.com>
2023-09-14 21:45:53 +0530

    Update app.py

--- web-apps/app.py
+++ web-apps/app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def hello_guvi_geek():
+    return render_template('index.html', message="Hello GUVI GEEK")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
