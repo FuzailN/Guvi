7e3ae024166027314692a2ef0cfa9e7180d0420a
FuzailN <129302212+FuzailN@users.noreply.github.com>
2023-09-14 21:26:20 +0530

    Update app.py

--- web-apps/app.py
+++ web-apps/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
